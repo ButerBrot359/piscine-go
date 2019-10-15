aea789bc6c4906c8535b65550ef3845e449fa8a8
ButerBrot359 <student@ALEM-a1dc04.alem.school>
2019-10-16 04:10:05 +0600

    GJKSOJDG

---
+++ test/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+
+	var number1, number2, number3 int = 1, 0, -1
+	if number1 >=0 {
+		z01.PrintRune('F')
+	} else if number1 < 0 {
+			z01.PrintRune('T')
+	}
+	z01.PrintRune('\n')
+	if number2 >=0 {
+		z01.PrintRune('F')
+	} else if number2 < 0 {
+			z01.PrintRune('T')
+	}
+	z01.PrintRune('\n')
+	if number3 >=0 {
+		z01.PrintRune('F')
+	} else if number3 < 0 {
+			z01.PrintRune('T')
+	}
+	z01.PrintRune('\n')
+
+}
