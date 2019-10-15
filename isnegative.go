5cadad868f67b0710b9c7f07da0c592607c959c0
buterbrot359 <student@ALEM-a1dc04.alem.school>
2019-10-16 04:41:19 +0600

    ggwrgv

--- isnegative.go
+++ isnegative.go
@@ -1,25 +1,12 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
-func main() {
+func isnegative(nb int) {
 
-	var number1, number2, number3 int = 1, 0, -1
-	if number1 >= 0 {
+	if nb >= 0  {
 		z01.PrintRune('F')
-	} else if number1 < 0 {
-		z01.PrintRune('T')
-	}
-	z01.PrintRune('\n')
-	if number2 >= 0 {
-		z01.PrintRune('F')
-	} else if number2 < 0 {
-		z01.PrintRune('T')
-	}
-	z01.PrintRune('\n')
-	if number3 >= 0 {
-		z01.PrintRune('F')
-	} else if number3 < 0 {
+	}else {
 		z01.PrintRune('T')
 	}
 	z01.PrintRune('\n')
