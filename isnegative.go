bb8efbcbd04e5e8549ca6e03e730d9bae8a03fb1
buterbrot359 <student@ALEM-a1dc04.alem.school>
2019-10-16 04:43:26 +0600

    ggwrgv

--- isnegative.go
+++ isnegative.go
@@ -4,9 +4,9 @@ import "github.com/01-edu/z01"
 
 func isnegative(nb int) {
 
-	if nb >= 0  {
+	if nb >= 0 {
 		z01.PrintRune('F')
-	}else {
+	} else {
 		z01.PrintRune('T')
 	}
 	z01.PrintRune('\n')
