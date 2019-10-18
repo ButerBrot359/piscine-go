ce495ef004151681509dc05ce000a99b225bc09a
buterbrot359 <student@ALEM-317038.alem.school>
2019-10-18 20:03:35 +0600

    ShmoxikIn

--- strrev.go
+++ strrev.go
@@ -1,10 +1,10 @@
 package piscine
 
-func StrRev(b string) string{
+func StrRev(b string) string {
 
 	var cox string
 	for _, geralt := range b {
-		cox = string(geralt) + cox  
+		cox = string(geralt) + cox
 	}
 	return cox
 }
