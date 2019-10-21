001dfa04974f43d2d6404c21b23dd418d72c0762
buterbrot359 <student@ALEM-317038.alem.school>
2019-10-21 12:39:56 +0600

    gtgtg

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -2,9 +2,9 @@ package piscine
 
 func RecursiveFactorial(nb int) int {
 
-	if nb == 1 || nb == 0 {
+	if nb == 1 {
 		return 1
-	} else if nb > 1 {
+	} else if nb > 0 && nb <= 20 {
 		return nb * RecursiveFactorial(nb-1)
 	} else {
 		return 0
