4dd91ac2b3f6ac11ed9f5d9bc249a435b236203a
buterbrot359 <student@ALEM-317038.alem.school>
2019-10-21 12:13:39 +0600

    iterative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,10 +1,14 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-
 	res := 1
-	for i := 1; i <= nb; i++ {
-		res = res * i
-	}
-	return res
+	if nb >= 0 {
+		for i := 1; i <= nb; i++ {
+			res = res * i
+		}
+		return res
+	} else {
+		res = 0
+		return res
+	} 	
 }
