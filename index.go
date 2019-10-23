efcb94fffe710bc5c8665f91ab59c45c6a868755
buterbrot359 <student@ALEM-7de628.alem.school>
2019-10-23 21:56:19 +0600

    lllklk

--- index.go
+++ index.go
@@ -1,34 +1,36 @@
 package piscine
 
-func Index(s, t string) int {
-	ln := 0
-	ln2 := 0
-	for _, c := range s {
-		if c == c {
-			ln++
-		}
+func Index(s string, toFind string) int {
+	newstr := []rune(s)
+	nb := StrLe(s)
+	strFind := []rune(toFind)
+	nbFind := StrLe(toFind)
+	match := 0
+	index := 0
+	if nbFind == 0 {
+		return 0
 	}
+	for i := 0; i < nbFind; i++ {
+		for j := index; j < nb; j++ {
+			match = 0
+			if strFind[i] == newstr[j] {
+				match = 1
+				index = j
+				break
+			}
 
-	for _, c := range t {
-		if c == c {
-			ln2++
+		}
+		if match == 0 {
+			return -1
 		}
 	}
-	for i := 0; i < ln; i++ {
-		if ln2 != 0 && s[i] == t[0] {
-			ok := true
-			cur_ch := 0
-			for j := 0; j < ln2; j++ {
-				if i+cur_ch >= ln || t[j] != s[i+cur_ch] {
-					ok = false
-					break
-				}
-				cur_ch++
-			}
-			if ok == true {
-				return i
-			}
-		}
+	return index - nbFind + 1
+}
+func StrLe(s string) int {
+	m := 0
+	for range s {
+		m++
 	}
-	return -1
+	return m
+
 }
