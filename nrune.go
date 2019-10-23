49b185c5dedc6cb70247e4857690c835bf48eb7a
buterbrot359 <student@ALEM-7de628.alem.school>
2019-10-23 17:46:21 +0600

    H&M

--- nrune.go
+++ nrune.go
@@ -2,5 +2,16 @@ package piscine
 
 func NRune(s string, n int) rune {
 	b := []rune(s)
+	nb := Len(s)
+	if n > nb {
+		return 0
+	}
 	return b[n-1]
+}
+func Len(s string) int {
+	m := 0
+	for range s {
+		m++
+	}
+	return m
 }
