46431009f5b4df09f40c119ec182bd807448fe41
buterbrot359 <student@ALEM-a1db51.alem.school>
2019-10-24 14:37:16 +0600

    ghghhg

---
+++ revparams/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	arg := os.Args
+	nb := NB(arg)
+	lol := []string(arg)
+	for i := nb - 1; i > 0; i-- {
+		for _, b := range lol[i] {
+			z01.PrintRune(b)
+		}
+		z01.PrintRune('\n')
+	}
+	
+}
+func NB(s []string) int {
+	m := 0
+	for range s {
+		m++
+	}
+	return m
+}
