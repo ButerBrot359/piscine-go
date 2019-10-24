d5b76f57f0275fe279fdfc65839c1787379d69a0
buterbrot359 <student@ALEM-a1db51.alem.school>
2019-10-24 22:47:25 +0600

    fdfdf

--- islower.go
+++ islower.go
@@ -7,7 +7,7 @@ func IsLower(str string) bool {
 	}
 	nbr := []rune(str)
 	for i := 0; i < m; i++ {
-		if (nbr[i] >= 'a' && nbr[i] <= 'z') {
+		if nbr[i] >= 'a' && nbr[i] <= 'z' {
 			continue
 		} else {
 			return false
