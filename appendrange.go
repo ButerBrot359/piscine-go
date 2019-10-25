bdc00cf853cf04796909a2908aae678da4ee502c
buterbrot359 <student@ALEM-a1db51.alem.school>
2019-10-25 21:13:23 +0600

    answer

--- appendrange.go
+++ appendrange.go
@@ -3,9 +3,9 @@ package piscine
 func AppendRange(s, b int) []int {
 	var answer []int
 
-	for i := 0; i < (b - s); i++ {
+	for i := s; i < b; i++ {
 
-		answer = append(answer, i+1)
+		answer = append(answer, i)
 	}
 	return answer
 }
